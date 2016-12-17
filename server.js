5c3612a4baaed3654b1eae0c25aed495166563fb
Jason Wilson <jason@wilsons.io>
2016-12-16 23:23:21 -0500

    Initial server test.

---
+++ server.js
@@ -0,0 +1,35 @@
+// server.js
+
+// congenial robot
+// Jason Wilson - jason@wilsons.io
+
+// Server setup to receive emails from the USCG Notice to Mariners list for
+// eventual processing into useful geo-referenced data streams.
+
+var express = require('express');
+var port = process.env.PORT || '8080'
+
+var server = express();
+
+// Logging middleware
+server.use( function timestamp ( req, res, next ) {
+  console.log( `Time: ${ Date.now() }` );
+  next();
+});
+
+// Incoming mail route
+server.post( 'brow', function( req, res ) {
+  
+  var parsedMessage = JSON.parse( req.body );
+  
+  console.log( `From: ${parsedMessage.headers['From']}` );
+  console.log( `Subject: ${parsedMessage.headers['Subject']}` );
+  
+  res.status(200).send( "Message received." );
+  
+});
+
+// Listen up
+server.listen( port );
+
+console.log(`Server is awaiting mail on port: ${port}`);
